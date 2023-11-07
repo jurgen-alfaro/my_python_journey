17764bb50579afc80d505aaf55f33f471561be9a
Jurgen Alfaro <dev.jurgen.alfaro@gmail.com>
2023-11-07 15:36:04 -0600

    added math function basics file

---
+++ the_basics/math_functions.py
@@ -0,0 +1,15 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi)) # round it to the nearest integer
+print(math.ceil(pi)) # round the number up
+print(math.floor(pi)) # round the number down
+print(abs(pi)) # gives the absolute value of the number
+print(pow(pi, 2)) # power function (exponential)
+print(math.sqrt(pi)) # square root function
+print(max(x, y, z)) # find the max number
+print(min(x, y, z)) # find the min number
