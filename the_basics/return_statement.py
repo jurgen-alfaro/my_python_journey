67dd3236cf28b5af3b888760375da174379ab66b
Jurgen Alfaro <dev.jurgen.alfaro@gmail.com>
2023-11-08 13:51:05 -0600

    added functions and return statement basics files

---
+++ the_basics/return_statement.py
@@ -0,0 +1,7 @@
+# Functions send Python values/objects back to the caller. 
+# These values/objects are known as the function’s return value
+
+def multiply(number1, number2):
+    return number1 * number2
+
+print(multiply(6, 8))
